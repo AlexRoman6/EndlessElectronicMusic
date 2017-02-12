ee2c00b591c55a88ddd2766cccf7fc0ab627df90
david <david160296@gmail.com>
2017-02-12 19:10:34 +0100

    modificaciones cancion

--- EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerSongsCategory.java
+++ EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerSongsCategory.java
@@ -1,13 +1,27 @@
 package dad.endlessElectronicMusic.web;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import antlr.collections.List;
+import dad.endlessElectronicMusic.entidades.Cancion;
+import dad.endlessElectronicMusic.entidades.CancionRepository;
 
 @Controller
 public class ControllerSongsCategory {
+	
+	@Autowired
+	CancionRepository repository;
 	
 	@RequestMapping("/songs-category")
 	public ModelAndView printSongCategory(HttpServletRequest request) {
@@ -17,5 +31,12 @@ public class ControllerSongsCategory {
 		return result;
 		
 	}
+	@GetMapping("(songs-category/{tipo}")
+	public String enlace(Model model, @RequestParam String tipo) {
+		List<Cancion> genero = new List<Cancion>();
+		genero = repository.findAll(new Sort("tipo"));
+		model.addAttribute("cancion", genero);
 
+		return "songs-category";
+	}
 }
