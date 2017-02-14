a1822401591fe34a197fb25609b88d99d0c21676
david <david160296@gmail.com>
2017-02-14 20:34:41 +0100

    casi acabado el son detail

--- EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerSongDetail.java
+++ EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerSongDetail.java
@@ -1,13 +1,16 @@
 package dad.endlessElectronicMusic.web;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import dad.endlessElectronicMusic.entidades.Artista;
@@ -18,12 +21,18 @@ import dad.endlessElectronicMusic.entidades.CancionRepository;
 @Controller
 public class ControllerSongDetail {
 	
+	@Autowired
+	private CancionRepository cancionRepository;
+	@Autowired
+	private ArtistaRepository artistaRepository;
 	
 	@RequestMapping("/songs-detail")
-	public ModelAndView printSongDetail(HttpServletRequest request) {
+	public ModelAndView printSongDetail(HttpServletRequest request, @RequestParam Long filter) {
 		ModelAndView result = new ModelAndView();
 		result.addObject("resources", request.getContextPath() + "/resources");
 		
+		Cancion cancion = cancionRepository.findOne(filter);
+		result.addObject("cancion", cancion);
 		return result;
 		
 	}
