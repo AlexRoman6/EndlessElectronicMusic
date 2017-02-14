2ccdd4d43f741217fc850df687fcd8632c472b68
AlexRoman6 <alexroman94@gmail.com>
2017-02-14 20:10:12 +0100

    Eventos y posts
    Se muestran correctamente en la página. Falta resolver el problema del
    lugar en los eventos.

--- EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerBlog.java
+++ EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerBlog.java
@@ -9,8 +9,6 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
