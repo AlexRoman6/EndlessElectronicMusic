8fd01b38465b37e617ce93963d6171cb2dc4f522
Fran <framortej@gmail.com>
2017-02-21 14:51:16 +0100

    Añadida la funcionalidad de comentar en los eventos y en los post

--- EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerBlogPost.java
+++ EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerBlogPost.java
@@ -7,16 +7,20 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import dad.endlessElectronicMusic.entidades.ComentarioEvento;
+import dad.endlessElectronicMusic.entidades.ComentarioEventoRepository;
 import dad.endlessElectronicMusic.entidades.ComentarioPost;
+import dad.endlessElectronicMusic.entidades.ComentarioPostRepository;
 import dad.endlessElectronicMusic.entidades.Evento;
 import dad.endlessElectronicMusic.entidades.EventoRepository;
 import dad.endlessElectronicMusic.entidades.Post;
 import dad.endlessElectronicMusic.entidades.PostRepository;
+import dad.endlessElectronicMusic.entidades.Usuario;
 import dad.endlessElectronicMusic.entidades.UsuarioRepository;
 
 @Controller
@@ -31,6 +35,13 @@ public class ControllerBlogPost {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
+	@Autowired
+	private ComentarioEventoRepository comentarioEvenRepo;
+	
+	@Autowired
+	private ComentarioPostRepository comentarioPostRepo;
+	
+	
 	private String sLogin = "Iniciar Sesión";
 	private String sRegister = "Registrarse";
 	private String sPathRegister = "user-register.html";
@@ -68,9 +79,46 @@ public class ControllerBlogPost {
 		}
 
 		result.addObject("type", type);
+		result.addObject("id", id);
 
 		return result;
 
 	}
+	
+	@PostMapping("/procesarComentario")
+	public ModelAndView printPost(HttpServletRequest request, HttpSession sesion, @RequestParam String type,
+			@RequestParam Long id) {
+		
+		ModelAndView result = new ModelAndView();
+		result.addObject("resources", request.getContextPath() + "/resources");
+		
+		Usuario temp = usuarioRepository.getOne(1L);
+		
+		if(type.equals("eventos")){
+			
+			ComentarioEvento ce = new ComentarioEvento(temp, request.getParameter("textoComentario"));
+			
+			ce.setEvento(repositoryEventos.getOne(id));
+			
+			comentarioEvenRepo.save(ce);
+			
+		}else{
+			ComentarioPost cp = new ComentarioPost(temp, request.getParameter("textoComentario"));
+			
+			cp.setPost(repositoryPost.getOne(id));
+			
+			comentarioPostRepo.save(cp);
+		}
+
+		
+		
+		result.addObject("type", type);
+		result.addObject("id", id);
+
+		return result;
+
+	}
+	
+	
 
 }
