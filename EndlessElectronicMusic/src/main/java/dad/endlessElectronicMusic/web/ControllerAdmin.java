01fd198bc1659db4d713e8691e8911e33b2aa98c
Fran <framortej@gmail.com>
2017-03-08 22:15:13 +0100

    Ya se puede añadir Artistas y canciones

--- EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerAdmin.java
+++ EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerAdmin.java
@@ -7,8 +7,25 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+import java.util.List;
+
+import dad.endlessElectronicMusic.entidades.Artista;
+import dad.endlessElectronicMusic.entidades.ArtistaRepository;
+import dad.endlessElectronicMusic.entidades.Cancion;
+import dad.endlessElectronicMusic.entidades.CancionRepository;
+import dad.endlessElectronicMusic.entidades.ComentarioEventoRepository;
+import dad.endlessElectronicMusic.entidades.ComentarioPostRepository;
+import dad.endlessElectronicMusic.entidades.EventoRepository;
+import dad.endlessElectronicMusic.entidades.Imagen;
+import dad.endlessElectronicMusic.entidades.ImagenRepository;
+import dad.endlessElectronicMusic.entidades.PostRepository;
 
 import dad.endlessElectronicMusic.entidades.Cancion;
 import dad.endlessElectronicMusic.entidades.CancionRepository;
@@ -17,6 +34,27 @@ import dad.endlessElectronicMusic.entidades.UsuarioRepository;
 
 @Controller
 public class ControllerAdmin {
+
+	@Autowired
+	private CancionRepository songRepo;
+
+	@Autowired
+	private PostRepository postRepo;
+
+	@Autowired
+	private EventoRepository eventRepo;
+
+	@Autowired
+	private ArtistaRepository artistRepo;
+
+	@Autowired
+	private ComentarioEventoRepository comenEvenRepo;
+
+	@Autowired
+	private ComentarioPostRepository comenPostRepo;
+
+	@Autowired
+	private ImagenRepository imageRepo;
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
@@ -43,6 +81,9 @@ public class ControllerAdmin {
 		ModelAndView result = new ModelAndView();
 		result.addObject("resources", request.getContextPath() + "/resources");
 
+		List<Artista> ls = artistRepo.findAll();
+		result.addObject("artistas", ls);
+
 		return result;
 
 	}
@@ -60,6 +101,55 @@ public class ControllerAdmin {
 	public ModelAndView printAdminAddPost(HttpServletRequest request) {
 		ModelAndView result = new ModelAndView();
 		result.addObject("resources", request.getContextPath() + "/resources");
+
+		return result;
+
+	}
+
+	@PostMapping("/admin/procesarFormulario")
+	public ModelAndView printProcesarForm(HttpServletRequest request, @RequestParam String type,
+			@RequestParam("fileUpload") MultipartFile file) {
+
+		ModelAndView result = new ModelAndView();
+		result.addObject("resources", request.getContextPath() + "/resources");
+
+		// String uploadPath = request.getContextPath() +
+		// "/resources/core/css/img/";
+		// String uploadPath = "D:\\";
+
+		if (type.equals("addArtista")) {
+			Artista a = new Artista(request.getParameter("nomArtista"), request.getParameter("nomNacionalidad"),
+					request.getParameter("nomEstilo"));
+			artistRepo.save(a);
+		}
+
+		if (type.equals("addSong")) {
+
+			// try {
+			//
+			// byte[] bytes = file.getBytes();
+			// Path path = Paths.get(uploadPath + file.getOriginalFilename());
+			// Files.write(path, bytes);
+			//
+			// } catch (IOException e) {
+			// e.printStackTrace();
+			// }
+
+			Imagen imagen1 = new Imagen("fotoDef.png");
+			imageRepo.save(imagen1);
+
+			Artista a = artistRepo.findOne(Long.parseLong(request.getParameter("idSong")));
+
+			String name = request.getParameter("nomSong");
+			String url = request.getParameter("url");
+			String estilo = request.getParameter("estiloSong");
+			int anio = Integer.parseInt(request.getParameter("anioSong"));
+
+			Cancion c = new Cancion(name, a, a.getNombre(), estilo, anio, new Date(), 0, imagen1, url);
+
+			songRepo.save(c);
+
+		}
 
 		return result;
 
