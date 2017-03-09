bbb2fbbd8f1a4846cfbbbb36768656640190a55a
AlexRoman6 <alexroman94@gmail.com>
2017-03-09 12:26:33 +0100

    gallery.html mostrado correctamente
    La galería de nuestra pagina de administración muestra las imágenes por
    el orden de su id.
    Sólo falta implementar el método que las elimina.

--- EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/entidades/Imagen.java
+++ EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/entidades/Imagen.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 
 @Entity
 public class Imagen {
@@ -14,16 +13,7 @@ public class Imagen {
 	private long id;
 
 	private String url;
-	/*
-	@OneToOne
-	private Cancion cancion;
-	
-	@OneToOne
-	private Evento evento;
-	
-	@OneToOne
-	private Post post;
-	*/
+
 	protected Imagen(){
 		
 	}
