d669a47b829f16bad2fbd2141bb109252aba89b4
Fran <framortej@gmail.com>
2017-03-19 12:27:37 +0100

    Versión final de del servicio de e-mails funcionando perfectamente. Toda esta dispuesto y funcionando en la web de azure. Fin de la fase 3

--- MailService/src/main/java/DAD/MailService/ConexionCliente.java
+++ MailService/src/main/java/DAD/MailService/ConexionCliente.java
@@ -4,13 +4,21 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.security.Security;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Properties;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.sun.mail.smtp.SMTPTransport;
 
 public class ConexionCliente extends Thread implements Observer {
 
@@ -33,6 +41,7 @@ public class ConexionCliente extends Thread implements Observer {
 		}
 	}
 
+	@SuppressWarnings("restriction")
 	@Override
 	public void run() {
 
@@ -40,55 +49,77 @@ public class ConexionCliente extends Thread implements Observer {
 		List<String> mensajeRecibido = new ArrayList<>();
 		boolean conectado = true;
 
-		try {
+		System.out.println("Holaaa");
 
-			System.out.println("Holaaa");
+		while (conectado) {
+			try {
+				mensajeRecibido = (List<String>) entradaDatos.readObject();
 
-			while (conectado) {
-				try {
-					mensajeRecibido = (List<String>) entradaDatos.readObject();
-					if (mensajeRecibido != null) {
-						salidaDatos.writeUTF("Datos recibidos con exito");
-						System.out.println(mensajeRecibido.get(0));
-						System.out.println(mensajeRecibido.get(1));
+				if (mensajeRecibido != null) {
 
-						ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Mail.xml");
-
-						EMail mm = (EMail) context.getBean("mailMail");
-						
-						try {
-							
-							mm.sendMail("Dadmusic17@gmail.com", mensajeRecibido.get(1), "¡¡Bienvenido a EEM!!", "Hola "
-									+ mensajeRecibido.get(0)
-									+ "\n\nTe damos la bienvenida a Endless Electronic Music. Esperemos que disfrutes de las mejores canciones del momento.\n\nUn saludo");
-							
-						} catch (Exception e) {
-							System.out.println("Error envio MAIL");
-							e.printStackTrace();
-						}
-						
-						salidaDatos.writeUTF("Correo Enviado Correctamente");
-
-					}
-
-				} catch (IOException ex) {
-					System.out.println("Cliente Desconectado");
-					conectado = false;
+					salidaDatos.writeUTF("Datos recibidos con exito");
+					System.out.println(mensajeRecibido.get(0));
+					System.out.println(mensajeRecibido.get(1));
 
 					try {
-						entradaDatos.close();
-						salidaDatos.close();
-					} catch (IOException ex2) {
+
+						Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+						final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+
+						// Get a Properties object
+						Properties props = System.getProperties();
+						props.setProperty("mail.smtps.host", "smtp.gmail.com");
+						props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+						props.setProperty("mail.smtp.socketFactory.fallback", "false");
+						props.setProperty("mail.smtp.port", "465");
+						props.setProperty("mail.smtp.socketFactory.port", "465");
+						props.setProperty("mail.smtps.auth", "true");
+
+						props.put("mail.smtps.quitwait", "false");
+
+						Session session = Session.getInstance(props, null);
+
+						final MimeMessage msg = new MimeMessage(session);
+
+						// -- Set the FROM and TO fields --
+						msg.setFrom(new InternetAddress("Dadmusic17@gmail.com"));
+						msg.setRecipients(Message.RecipientType.TO,
+								InternetAddress.parse(mensajeRecibido.get(1), false));
+
+						msg.setSubject("¡¡Bienvenido a EEM!!");
+						msg.setText(
+								"Hola " + mensajeRecibido.get(0)
+										+ "\n\nTe damos la bienvenida a Endless Electronic Music. Esperemos que disfrutes de las mejores canciones del momento.\n\nUn saludo",
+								"utf-8");
+						msg.setSentDate(new Date());
+
+						SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
+
+						t.connect("smtp.gmail.com", "Dadmusic17@gmail.com", "endless2017");
+						t.sendMessage(msg, msg.getAllRecipients());
+						t.close();
+						conectado = false;
+						try {
+							entradaDatos.close();
+							salidaDatos.close();
+						} catch (IOException ex2) {
+						}
+
+					} catch (MessagingException ex) {
+						System.out.println("Cliente Desconectado");
+						conectado = false;
 					}
 				}
+
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Fallo creacion List");
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Fallo creacion List");
-			e.printStackTrace();
 		}
-
 	}
 
 	@Override
