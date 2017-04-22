8804d3795aa3cb081770cd203012b55db4fcf485
Fran <framortej@gmail.com>
2017-04-22 19:08:53 +0200

    Adaptada la aplicacion para el uso de un servidor rest

--- EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerUserRegister.java
+++ EndlessElectronicMusic/src/main/java/dad/endlessElectronicMusic/web/ControllerUserRegister.java
@@ -4,19 +4,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import dad.endlessElectronicMusic.entidades.Usuario;
 import dad.endlessElectronicMusic.entidades.UsuarioRepository;
-import dad.endlessElectronicMusic.socket.MailSender;
 
 @Controller
 public class ControllerUserRegister {
+
+	@Value("${dad.servicio.url}")
+	private String urlServicio;
 
 	@Autowired
 	private UsuarioRepository userRepo;
@@ -25,10 +29,10 @@ public class ControllerUserRegister {
 	public ModelAndView printUserRegister(HttpServletRequest request, HttpSession sesion) {
 		ModelAndView result = new ModelAndView();
 		result.addObject("resources", request.getContextPath() + "/resources");
-		
-		CsrfToken token = (CsrfToken) request.getAttribute("_csrf"); 
+
+		CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
 		result.addObject("token", token.getToken());
-		
+
 		ControllerIndex.renderUsuarios(request, result);
 
 		return result;
@@ -39,8 +43,20 @@ public class ControllerUserRegister {
 	public String newUser(@RequestParam String userReg, @RequestParam String mailReg, @RequestParam String passReg) {
 
 		Usuario u = new Usuario(userReg, mailReg, passReg, true, false);
+
+		int arroba = mailReg.indexOf("@");
+		String uMail = mailReg.substring(0, arroba);
+		String ext = mailReg.substring(arroba+1, mailReg.length());
+
+		int punto = ext.indexOf(".");
+		String server = ext.substring(0, punto);
+		ext = ext.substring(punto + 1, ext.length());
 		
-		MailSender.mailSender(userReg, mailReg);
+		
+		String urlFinal = urlServicio + "/user/" + userReg + "/mail/" + uMail + "/"+ server +"/" + ext;
+
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getForObject(urlFinal, String.class);
 
 		userRepo.save(u);
 
@@ -49,9 +65,8 @@ public class ControllerUserRegister {
 
 	@RequestMapping("/login")
 	public String login() {
-		
-		
+
 		return "redirect:/user-register.html";
 	}
-	
+
 }
